9943f184c72f5407746ee15101f964b9a25bf6d2
Jecca Arthur <arthurj@my.lanecc.edu>
2020-11-19 14:26:04 -0800

    Added note

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />     {/* this is a component */}
   </React.StrictMode>,
   document.getElementById('root')
 );
